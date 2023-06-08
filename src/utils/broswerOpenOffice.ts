7df4ab0983bce1add18d308fbb96da2aad1d63c5
dongling.wen <dongling.wen@corp.to8to.com>
2023-06-08 14:37:24 +0800

    浏览器打开office文件

---
+++ src/utils/broswerOpenOffice.ts
@@ -0,0 +1,4 @@
+// url例子 : https://pic.to8to.com/te/supply/63166dc6e5f64845881ec5a17081990c.xlsx
+export default function browserOpenOffice(url) {
+  window.open(`https://view.officeapps.live.com/op/view.aspx?src=${url}`);
+}
