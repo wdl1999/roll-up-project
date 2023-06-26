d5b054e0d55fcaad56347c2550ce37a5c148acda
dongling.wen <dongling.wen@corp.to8to.com>
2023-06-26 15:45:18 +0800

    merge

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
-<<<<<<< HEAD
-·
-=======
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [createVuePlugin()],
     server: {
-      port: 8080
+      port: 8080,
     },
     preview: {
       port: 8080,
-      https: true
+      https: true,
     },
     resolve: {
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.d.ts']
-    }
-  }
-})
->>>>>>> a27f0732d2452835f12ea015798c505a92583d5e
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".d.ts"],
+    },
+  };
+});
